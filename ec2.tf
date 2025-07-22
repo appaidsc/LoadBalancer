0365839e641be2b6fb803543e17bd32380ac88d7
appaidsc <165121961+appaidsc@users.noreply.github.com>
2025-07-22 15:43:41 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,51 @@
-resource "aws_key_pair" "my_key_pair" {
-  key_name   = "my-key-pair"
-  public_key = file("~/.ssh/id_rsa.pub")  # Update path to your public key file
+data "aws_vpc" "default" {
+  default = true
 }
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2-http-sg"
+  description = "Allow HTTP and SSH"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "web" {
   count         = 2
-  ami           = "ami-0437df53acb2bbbfd"  # Amazon Linux 2 (us-east-1)
+  ami           = "ami-0437df53acb2bbbfd" # Amazon Linux 2 (us-east-1)
   instance_type = "t3.micro"
+  subnet_id     = element(data.aws_subnets.default.ids, count.index)
   key_name      = aws_key_pair.my_key_pair.key_name
+
+  security_groups = [aws_security_group.ec2_sg.name]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -14,10 +53,10 @@ resource "aws_instance" "web" {
               yum install -y httpd
               systemctl start httpd
               systemctl enable httpd
-              echo "<h1>Server $(hostname -f) is up</h1>" > /var/www/html/index.html
+              echo "<h1>Deployed by Terraform on instance $(hostname -f)</h1>" > /var/www/html/index.html
             EOF
 
   tags = {
-    Name = "Terraform-Instance-${count.index + 1}"
+    Name = "WebServer-${count.index + 1}"
   }
 }
