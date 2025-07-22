856b36e92e3ccb42ff21222046f5b1ae5f41cb0c
appaidsc <165121961+appaidsc@users.noreply.github.com>
2025-07-22 15:34:31 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,12 +2,20 @@ resource "aws_key_pair" "my_key_pair" {
   key_name   = "my-key-pair"
   public_key = file("~/.ssh/id_rsa.pub")  # Update path to your public key file
 }
-
 resource "aws_instance" "web" {
   count         = 2
-  ami           = "ami-0437df53acb2bbbfd"  # Amazon Linux 2 in us-east-1
-  instance_type = "t2.micro"
+  ami           = "ami-0437df53acb2bbbfd"  # Amazon Linux 2 (us-east-1)
+  instance_type = "t3.micro"
   key_name      = aws_key_pair.my_key_pair.key_name
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo "<h1>Server $(hostname -f) is up</h1>" > /var/www/html/index.html
+            EOF
 
   tags = {
     Name = "Terraform-Instance-${count.index + 1}"
