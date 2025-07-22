4ed32d6c39965d8898350c7f5f288b0207de1ef5
appaidsc <165121961+appaidsc@users.noreply.github.com>
2025-07-22 15:39:49 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-  required_version = ">= 1.0"
-}
-
 provider "aws" {
   region = "eu-north-1"  # Change to your preferred region
 }
