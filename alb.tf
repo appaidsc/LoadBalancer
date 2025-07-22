8efe075915c2b73685fb1587996eb382302012cd
appaidsc <165121961+appaidsc@users.noreply.github.com>
2025-07-22 15:44:32 +0530

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -1,73 +1,33 @@
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
-resource "aws_security_group" "alb_sg" {
-  name        = "allow_http"
-  description = "Allow HTTP inbound traffic"
-  vpc_id      = data.aws_vpc.default.id
-
-  ingress {
-    description      = "HTTP from anywhere"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_lb" "app_lb" {
-  name               = "app-load-balancer"
+resource "aws_lb" "web_alb" {
+  name               = "web-alb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg.id]
+  security_groups    = [aws_security_group.ec2_sg.id]
   subnets            = data.aws_subnets.default.ids
 }
 
-resource "aws_lb_target_group" "tg" {
-  name     = "app-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = data.aws_vpc.default.id
-
-  health_check {
-    path                = "/"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 5
-    unhealthy_threshold = 2
-    matcher             = "200-399"
-  }
+resource "aws_lb_target_group" "web_tg" {
+  name        = "web-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = data.aws_vpc.default.id
+  target_type = "instance"
 }
 
-resource "aws_lb_listener" "listener" {
-  load_balancer_arn = aws_lb.app_lb.arn
+resource "aws_lb_listener" "web_listener" {
+  load_balancer_arn = aws_lb.web_alb.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.tg.arn
+    target_group_arn = aws_lb_target_group.web_tg.arn
   }
 }
 
-resource "aws_lb_target_group_attachment" "tg_attachment" {
+resource "aws_lb_target_group_attachment" "attach" {
   count            = 2
-  target_group_arn = aws_lb_target_group.tg.arn
+  target_group_arn = aws_lb_target_group.web_tg.arn
   target_id        = aws_instance.web[count.index].id
   port             = 80
 }
